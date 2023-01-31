c0ceb6d778d3372f82361a1c203be82e22495de8
Kim Robert Rozen <rozenkimrobert@gmail.com>
2023-01-31 13:21:45 +0800

    Add project files.

---
+++ EcommWebAPIDataAccess/DbContexts/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommWebAPIDataAccess.DbContexts
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) 
+        {
+
+        }
+
+
+    }
+}
